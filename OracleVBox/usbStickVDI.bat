98cecde723e4a6f67095b3b9b34bc78a64f4bc36
Deyan Dobromirov <dvd_video@abv.bg>
2018-12-06 10:30:35 +0200

    Removed oracle VB installation path printout

--- OracleVBox/usbStickVDI.bat
+++ OracleVBox/usbStickVDI.bat
@@ -4,8 +4,6 @@ set usbPath=%~dp0
 set usbDisk=%1
 set usbVBox=%programfiles%\Oracle\VirtualBox\
 
-echo %usbVBox%
-
 call "%usbVBox%VBoxManage.exe" internalcommands createrawvmdk -filename "%usbPath%usb.vmdk" -rawdisk \\.\PhysicalDrive%usbDisk%
 
 timeout 500
