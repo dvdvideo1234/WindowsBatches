6989124c20eb522ec63fffb26fb2217b6cd5cf0a
Deyan Dobromirov <dvd_video@abv.bg>
2018-12-08 20:19:34 +0200

    Added: Extra commands

--- usbStickDriverWIM/apply.bat
+++ usbStickDriverWIM/apply.bat
@@ -1,21 +1,28 @@
+:: dism /cleanup-wim
+:: dism /get-mountedwiminfo
+:: Imagex /unmount %dismDIRM%
+:: HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WIMMount\Mounted Images\
+
 @echo off
 
 set dismPath=%~dp0
 set dismFile=%1
 set dismID=%2
 set dismTime=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+set dismDIRM=%dismPath%Mount1
 
 if not exist "%dismPath%Backup\" mkdir %dismPath%Backup
-rmdir /Q /S %dismPath%Mount
-mkdir %dismPath%Mount
+if not exist "%dismDIRM%\"  mkdir %dismDIRM%
 
 echo F|xcopy "%dismPath%%dismFile%.wim" "%dismPath%Backup/%dismFile%$%dismTime%.wim"
 
-call dism /mount-wim /wimfile:%dismPath%%dismFile%.wim /index:%dismID% /mountdir:%dismPath%Mount
-call dism /image:%dismPath%Mount /add-driver:"%dismPath%Drivers" /recurse
-call dism /unmount-wim /mountdir:%dismPath%Mount /commit
+call dism /mount-wim /wimfile:%dismPath%%dismFile%.wim /index:%dismID% /mountdir:%dismDIRM%
+call dism /image:%dismDIRM% /add-driver:"%dismPath%Drivers" /recurse
+call dism /unmount-wim /mountdir:%dismDIRM% /commit
+::call dism /unmount-wim /mountdir:%dismDIRM% /discard
 
-echo Remove directory: %dismPath%Mount
-rmdir %dismPath%Mount
+
+:: rmdir %dismDIRM% /s /q
+echo Remove mount: %dismDIRM%
 
 timeout 500
