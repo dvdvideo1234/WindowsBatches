4f927494dd4ca1c2edc723dd5778f9e82eaf6df6
Deyan Dobromirov <dvd_video@abv.bg>
2019-04-20 21:57:19 +0300

    Rename variable mgrPVDI

--- OracleVBox/resizeVDI.bat
+++ OracleVBox/resizeVDI.bat
@@ -1,9 +1,9 @@
 @echo off
 
 set mgrPath=%programfiles%\Oracle\VirtualBox\
-set mgrVDIR=F:\Virtuals\UbuntuLFD\UbuntuLFD.vdi
+set mgrPVDI=F:\Virtuals\UbuntuLFD\UbuntuLFD.vdi
 set mgrSIZE=%1
 
-call "%mgrPath%VBoxManage.exe" modifyhd "%mgrVDIR%" --resize %mgrSIZE%
+call "%mgrPath%VBoxManage.exe" modifyhd "%mgrPVDI%" --resize %mgrSIZE%
 
 timeout 500
