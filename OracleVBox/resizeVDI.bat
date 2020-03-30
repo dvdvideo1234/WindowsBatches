c71f6e2a108c03b72a0c9be53cb3d7aba95d18a7
Deyan Dobromirov <dvd_video@abv.bg>
2020-03-30 14:27:47 +0300

    Updated: Resize all the virtual drive images in the current folder

--- OracleVBox/resizeVDI.bat
+++ OracleVBox/resizeVDI.bat
@@ -1,9 +1,19 @@
 @echo off
 
 set mgrPath=%programfiles%\Oracle\VirtualBox\
-set mgrPVDI=F:\Virtuals\UbuntuLFD\UbuntuLFD.vdi
-set mgrSIZE=%1
 
-call "%mgrPath%VBoxManage.exe" modifyhd "%mgrPVDI%" --resize %mgrSIZE%
+for %%i in (*.vdi) do (
+  set mgrMB=
+  set /p mgrMB="New size in MB ( %%i ): "
+
+  IF DEFINED mgrMB (
+    echo "Resizing %%i to %mgrMB% MB..."
+    
+    call "%mgrPath%VBoxManage.exe" modifyhd "%%i" --resize "%mgrMB%"
+    
+  ) ELSE (
+    echo "Skipping %%i due to user request !"
+  )
+)
 
 timeout 500
