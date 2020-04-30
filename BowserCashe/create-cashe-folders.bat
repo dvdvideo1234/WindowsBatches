3635a36433c8b8571a495d719c33a90e51776e2e
Deyan Dobromirov <dvd_video@abv.bg>
2020-04-30 18:56:54 +0300

    Added: Folder auto-create on startup

---
+++ BowserCashe/create-cashe-folders.bat
@@ -0,0 +1,16 @@
+:: Switch drive
+K:
+
+:: Switch folder
+cd "K:\"
+
+:: Populate path
+md CACHEFILES
+cd CACHEFILES
+
+md "OPERA"
+md "OPERAGX"
+md "FIREFOX"
+md "INTERNET EXPLORER"
+
+exit 0
--- BowserCashe/opera.bat
+++ BowserCashe/opera.bat
@@ -1,7 +1,2 @@
-md "K:\CACHEFILES"
-
-md "K:\CACHEFILES\OPERA"
-md "K:\CACHEFILES\OPERAGX"
-
 mklink /d "C:\Users\%1\AppData\Local\Opera Software\Opera Stable" "K:\CACHEFILES\OPERA"
 mklink /d "C:\Users\%1\AppData\Local\Opera Software\Opera GX Stable" "K:\CACHEFILES\OPERAGX"
