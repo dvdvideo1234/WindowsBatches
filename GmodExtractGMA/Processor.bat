e23310ec0f8100e037554d1d3f2fe8b649325bb9
Deyan Dobromirov <dvd_video@abv.bg>
2020-06-13 15:25:55 +0300

    Fixed: Processor output logs overwrite

--- GmodExtractGMA/Processor.bat
+++ GmodExtractGMA/Processor.bat
@@ -1,4 +1,4 @@
 @echo off
 rd /S /Q %~f3\%~n2
-call %~f1/gmad.exe extract -file "%~f2" -out "%~f3\%~n2" > "%~f3\processor.log"
+call %~f1/gmad.exe extract -file "%~f2" -out "%~f3\%~n2" >> "%~f3\processor.log"
 echo > %~f3\%~n2\addon.txt
