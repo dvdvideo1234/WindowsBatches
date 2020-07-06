19237d73b809849070c671cb8d50dfb1c38299e0
Deyan Dobromirov <dvd_video@abv.bg>
2020-07-06 10:29:14 +0300

    Razer synapse starter

---
+++ RazerSynapse/synapse-start.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set "synapsePTH=C:\Program Files (x86)\Razer\Synapse3\"
+set "synapseSUB=WPFUI\Framework\Razer Synapse 3 Host\"
+set "synapseEXE=Razer Synapse 3.exe"
+
+call "%synapsePTH%%synapseSUB%%synapseEXE%" /StartMinimized
+
+exit 0
