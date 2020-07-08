5a93369b4508c1454f39d056100049b894632e38
Deyan Dobromirov <dvd_video@abv.bg>
2020-07-08 16:03:27 +0300

    Added: User questions control and better OS version

--- ResetCeticatesSPC/setup.bat
+++ ResetCeticatesSPC/setup.bat
@@ -10,21 +10,32 @@ FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windo
   set "OS_VERSION=%OS_VERSION% %%i %%j %%k"
 )
 
-FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v CurrentVersion`) DO (
+FOR /F "usebackq tokens=3" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v CurrentVersion`) DO (
   set "OS_VERSION=%OS_VERSION% %%i"
 )
 
-FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v CurrentBuild`) DO (
-  set "OS_VERSION=%OS_VERSION% %%i"
+FOR /F "usebackq tokens=3" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v CurrentBuild`) DO (
+  set "OS_VERSION=%OS_VERSION%.%%i"
 )
 
-FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v CSDBuildNumber`) DO (
-  set "OS_VERSION=%OS_VERSION% %%i"
+FOR /F "usebackq tokens=3" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v CSDBuildNumber`) DO (
+  set "OS_VERSION=%OS_VERSION% [%%i]"
 )
 
 echo %OS_VERSION%
+echo Please run the script as administrator to apply the registry changes!
 
-REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\SystemCertificates\SPC\Certificates /f
+set /p REG_CHANGE="Apply registry changes [y/N] ? "
+
+if /I "%REG_CHANGE%" EQU "y" (
+  set "REG_CHANGE=Y"
+)
+
+if /I "%REG_CHANGE%" EQU "Y" (
+  REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\SystemCertificates\SPC\Certificates /f
+) else (
+  echo Registry modification is needed to complete the process!
+)
 
 timeout 100
 
