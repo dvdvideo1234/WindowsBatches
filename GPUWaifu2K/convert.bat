088f33eb840590ec216d55f063544c5f7aa1dd0b
Deyan Dobromirov <dvd_video@abv.bg>
2020-09-28 23:28:14 +0300

    Added: Size reduction via FFMpeg

--- GPUWaifu2K/convert.bat
+++ GPUWaifu2K/convert.bat
@@ -16,7 +16,17 @@ setlocal EnableDelayedExpansion
 :: -x                   enable tta mode
 :: -f format            output image format (jpg/png/webp, default=ext/png)
 
-:: Manual
+:: https://github.com/BtbN/FFmpeg-Builds/releases
+:: -compression_level Values in [0,12]  (Higher is greater quality is reduced)
+:: -quality           Values in [0,100] (Higher is better)
+
+:: Manual FFMpeg
+
+set "waifuFFMpg=D:\FFmpeg\bin"
+set "waifuFFCom=2"
+set "waifuQualy=100"
+
+:: Manual Waifu2X
 
 set "waifuBase=D:\waifu2x-ncnn-vulkan"
 set "waifuExec=waifu2x-ncnn-vulkan"
@@ -27,8 +37,8 @@ set "waifuTile=128"
 set "waifuModel=cunet"
 set "waifuGPUID=0"
 
-:: Automatic
-
+:: Automatic ( Dont touch )
+set "waifuLogs=waifu.log"
 set "waifuFile=%1"
 set "waifuConv=%2"
 set "waifuCurr=%~dp0"
@@ -47,20 +57,20 @@ if %waifuConv% leq 1 (
 )
 
 if %waifuConv% equ 1 (
-  %waifuBase%\%waifuExec%.exe -v -i %waifuFile% -o out%waifuExtIMG% -m models-%waifuModel% -n %waifuNoise% -s %waifuScale% -t %waifuTile% -g %waifuGPUID% 
+  %waifuBase%\%waifuExec%.exe -v -i %waifuFile% -o out%waifuExtIMG% -m models-%waifuModel% -n %waifuNoise% -s %waifuScale% -t %waifuTile% -g %waifuGPUID% 1> %waifuLogs% 2>&1
 ) else (
   if not exist %waifuTemp% mkdir %waifuTemp%
 
   set /a "waifuCnt1=1"
   set /a "waifuCnt2=2"
   
-  %waifuBase%\%waifuExec%.exe -v -i %waifuFile% -o %waifuCurr%%waifuTemp%\1%waifuExtIMG% -m models-%waifuModel% -n %waifuNoise% -s %waifuScale% -t %waifuTile% -g %waifuGPUID%
+  %waifuBase%\%waifuExec%.exe -v -i %waifuFile% -o %waifuCurr%%waifuTemp%\1%waifuExtIMG% -m models-%waifuModel% -n %waifuNoise% -s %waifuScale% -t %waifuTile% -g %waifuGPUID% 1> %waifuLogs% 2>&1
   
   for /l %%k in (2, 1, %waifuConv%) do (
     echo.
     echo From: !waifuCurr!!waifuTemp!\!waifuCnt1!%waifuExtIMG%
     echo Dest: !waifuCurr!!waifuTemp!\!waifuCnt2!%waifuExtIMG%
-    %waifuBase%\%waifuExec%.exe -v -i !waifuCurr!!waifuTemp!\!waifuCnt1!%waifuExtIMG% -m models-%waifuModel% -o !waifuCurr!!waifuTemp!\!waifuCnt2!%waifuExtIMG% -n %waifuNoise% -s %waifuScale% -t %waifuTile% -g %waifuGPUID%
+    %waifuBase%\%waifuExec%.exe -v -i !waifuCurr!!waifuTemp!\!waifuCnt1!%waifuExtIMG% -m models-%waifuModel% -o !waifuCurr!!waifuTemp!\!waifuCnt2!%waifuExtIMG% -n %waifuNoise% -s %waifuScale% -t %waifuTile% -g %waifuGPUID% 1>> %waifuLogs% 2>>&1
 
     set /a "waifuCnt1+=1"
     set /a "waifuCnt2+=1"
@@ -69,4 +79,23 @@ if %waifuConv% equ 1 (
   copy /v !waifuCurr!!waifuTemp!\!waifuCnt1!%waifuExtIMG% %waifuCurr%\out%waifuExtIMG%
 )
 
+if defined waifuFFMpg (
+  %waifuFFMpg%\ffmpeg.exe -y -i out%waifuExtIMG% -compression_level %waifuFFCom% out_%waifuFFCom%%waifuExtIMG% 1>> %waifuLogs% 2>>&1
+  call :waifuGetRatio %waifuFile% out_%waifuFFCom%%waifuExtIMG%
+  echo.
+  echo FFMpeg... Output size is !waifuRatio!%% of input size^^!
+)
+
 timeout 100
+
+goto :eof
+
+:: Functions
+
+:waifuGetRatio
+echo %1 %~z1
+echo %2 %~z2
+set /A waifuRatio=0
+set /A "waifuRatio=%~z2*100"
+set /A "waifuRatio/=%~z1"
+goto :eof
