6b3f04fcb9fbb1b4ec905b884938acbf8cd738bd
Deyan Dobromirov <dvd_video@abv.bg>
2021-09-21 16:23:52 +0300

    Added: pool specific

--- CryptoMining/ETH/flexpool/start.bat
+++ CryptoMining/ETH/flexpool/start.bat
@@ -3,5 +3,6 @@ set "mineName=PhenomII"
 set "mineWall=0x5f85dc88f23636B100d8C015171021cDDA7C369d"
 set "mineBase=stratum+ssl://eth-de.flexpool.io:5555"
 set "mineBack=stratum+ssl://eth-se.flexpool.io:5555"
+set "mineAlloc=4078"
 
-call %minePath%\teamredminer.exe -a ethash -o %mineBase% -u %mineWall%.%mineName% -o %mineBack% -u %mineWall%.%mineName% -p x --eth_stratum ethproxy
+call %minePath%\teamredminer.exe -a ethash -o %mineBase% -u %mineWall%.%mineName% -o %mineBack% -u %mineWall%.%mineName% -p x --eth_4g_max_alloc=%mineAlloc% --eth_stratum ethproxy
