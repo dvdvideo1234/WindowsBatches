769cf4c665304625b618f202d32b46bc10a87b5b
Deyan Dobromirov <dvd_video@abv.bg>
2021-09-21 11:52:06 +0300

    Updated: Use variable to set pool wallet and worker

--- CryptoMining/ETH/start.bat
+++ CryptoMining/ETH/start.bat
@@ -1,3 +1,7 @@
 set "minePath=E:\Backup\Install\EtheriumMiner\teamredminer-v0.8.5-win"
+set "mineName=PhenomII"
+set "mineWall=0x5f85dc88f23636B100d8C015171021cDDA7C369d"
+set "mineBase=stratum+ssl://eth-de.flexpool.io:5555"
+set "mineBack=stratum+ssl://eth-se.flexpool.io:5555"
 
-call %minePath%\teamredminer.exe -a ethash -o stratum+ssl://eth-de.flexpool.io:5555 -u 0x5f85dc88f23636B100d8C015171021cDDA7C369d.PhenomII -o stratum+ssl://eth-se.flexpool.io:5555 -u 0x5f85dc88f23636B100d8C015171021cDDA7C369d.PhenomII -p x --eth_stratum ethproxy
+call %minePath%\teamredminer.exe -a ethash -o %mineBase% -u %mineWall%.%mineName% -o %mineBack% -u %mineWall%.%mineName% -p x --eth_stratum ethproxy
