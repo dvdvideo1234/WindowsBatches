a867adb635106fb3801302bab947c13f1e5b1ed2
Deyan Dobromirov <dvdvideo123@gmail.com>
2022-02-05 22:52:51 +0200

    Fixed: SPC cleaner throwing query error in the console Fixed: SPC cleaner checks for elevated command prompt

--- RegistryMods/ResetCeticatesSPC.bat
+++ RegistryMods/ResetCeticatesSPC.bat
@@ -1,49 +1,56 @@
 @echo off
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 set "OS_VERSION=Microsoft"
 
 for /F "usebackq tokens=3,4,5" %%i in (`REG QUERY "hklm\software\microsoft\windows NT\CurrentVersion" /v ProductName`) do (
-  set "OS_VERSION=%OS_VERSION% %%i %%j %%k"
+  set "OS_VERSION=!OS_VERSION! %%i %%j %%k"
 )
 
 for /F "usebackq tokens=3" %%i in (`REG QUERY "hklm\software\microsoft\windows NT\CurrentVersion" /v CurrentVersion`) do (
-  set "OS_VERSION=%OS_VERSION% %%i"
+  set "OS_VERSION=!OS_VERSION! %%i"
 )
 
 for /F "usebackq tokens=3" %%i in (`REG QUERY "hklm\software\microsoft\windows NT\CurrentVersion" /v CurrentBuild`) do (
-  set "OS_VERSION=%OS_VERSION%.%%i"
+  set "OS_VERSION=!OS_VERSION!.%%i"
 )
 
-echo %OS_VERSION%
-echo Please run the script as administrator to apply the registry changes!
+net session >nul 2>nul
+if !ERRORLEVEL! NEQ 0 (
+  echo Run the script as administrator to apply the registry changes^^!
+  goto end-script
+)
+
+echo Version: !OS_VERSION!
 
 set /p REG_CHANGE="Apply registry changes [y/N] ? "
 
-if /I "%REG_CHANGE%" EQU "y" (
+if /I "!REG_CHANGE!" EQU "y" (
   set "REG_CHANGE=Y"
 )
 
-set /p REG_FAVOR="Save to favorites [y/N] ? "
+set /p `REG_FAVOR="Save to favorites [y/N] ? "`
 
 if /I "%REG_FAVOR%" EQU "y" (
   set "REG_FAVOR=Y"
 )
 
-if /I "%REG_FAVOR%" EQU "Y" (
+if /I "!REG_FAVOR!" EQU "Y" (
   REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites ^
     /v "SPC" /t REG_SZ /d "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\SystemCertificates\SPC" /f
 )
 
-REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\SystemCertificates\SPC\Certificates >nul 2>&1
-
-if %ERRORLEVEL% EQU 0 (
-  if /I "%REG_CHANGE%" EQU "Y" (
+if /I "!REG_CHANGE!" EQU "Y" (
+  echo Registry modification requested^^!
+  REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\SystemCertificates\SPC\Certificates >NUL 2>NUL && (
+    echo Registry modification completed^^!
     REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\SystemCertificates\SPC\Certificates /f
-  ) else (
-    echo Registry modification is needed to complete the process!
   )
 ) else (
-  echo Modification is already done!
+  echo Registry modification canceled^^!
 )
+
+:end-script
 
 timeout 100
