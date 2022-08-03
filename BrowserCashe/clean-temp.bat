ca343aaf2e97ab75fce8ffe1be45763c083fd275
Deyan Dobromirov <dvdvideo123@gmail.com>
2022-08-03 23:26:12 +0300

    Added: Temp directories clean

---
+++ BrowserCashe/clean-temp.bat
@@ -0,0 +1,22 @@
+cd /d "C:%HOMEPATH%\local settings\temp"
+del c*.tmp
+
+cd /d "C:%HOMEPATH%\local settings\tmp"
+del c*.tmp
+
+cd /d %TMP%
+del c*.tmp
+
+cd /d %TEMP%
+del c*.tmp
+
+cd /d  C:\windows\temp
+del c*.*
+
+cd /d C:\tmp
+del c*.*
+
+cd /d C:\temp
+del c*.*
+
+exit 0
