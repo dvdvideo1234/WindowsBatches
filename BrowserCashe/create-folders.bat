46e4bebe5a55d202daeee06f5a32efee7bdb84fb
Deyan Dobromirov <dvdvideo123@gmail.com>
2022-08-15 12:25:04 +0300

    Renamed: Internet explorer folder to `IEXPLORER`

--- BrowserCashe/create-folders.bat
+++ BrowserCashe/create-folders.bat
@@ -1,20 +1,21 @@
 :: Switch drive folder
-cd /d %1
-
-:: cd /d E:\GIT\WindowsBatches\BrowserCashe
-:: create-folders.bat "V:\"
+cd /d "%1"
 
 :: Populate root
 if not exist "TEMP" mkdir "TEMP"
 
-:: Populate cache
+:: Populate one drive
+if not exist "ONEDRIVE" mkdir "ONEDRIVE"
+
+:: Populate cache for browsers
 if not exist "CACHEFILES" mkdir "CACHEFILES"
 cd "CACHEFILES"
 
-if not exist "OPERA"    mkdir "OPERA"
-if not exist "OPERAGX"  mkdir "OPERAGX"
-if not exist "FIREFOX"  mkdir "FIREFOX"
-if not exist "CHROME"   mkdir "CHROME"
-if not exist "INTERNET EXPLORER" mkdir "INTERNET EXPLORER"
+if not exist "EDGE" mkdir "EDGE"
+if not exist "OPERA" mkdir "OPERA"
+if not exist "CHROME" mkdir "CHROME"
+if not exist "OPERAGX" mkdir "OPERAGX"
+if not exist "FIREFOX" mkdir "FIREFOX"
+if not exist "IEXPLORER" mkdir "IEXPLORER"
 
 exit 0
