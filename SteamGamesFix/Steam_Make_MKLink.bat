18d39fbb84f26b7537069ff1814d68e5c0921bbe
Deyan Dobromirov <dvdvideo123@gmail.com>
2022-10-24 16:26:28 +0300

    Added: Creating steam MK link for the game that required to be installed on C:\

---
+++ SteamGamesFix/Steam_Make_MKLink.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set "SteamAppPWD=%CD%"
+set "SteamAppExe="
+set "SteamAppPth="
+
+for /F "Tokens=1,2*" %%A in ('reg query HKCU\SOFTWARE\Valve\Steam') do (
+    If "%%A" equ "SteamExe" set "SteamAppExe=%%C"
+    If "%%A" equ "SteamPath" set "SteamAppPth=%%C")
+
+echo Steam MKLink created for %SteamAppPth%
+
+:: cd "<GameFolder>”
+:: mklink "steam.exe" "<SteamFolder>\steam.exe"
+
+cd /d "%SteamAppPWD%"
+mklink "steam.exe" "%SteamAppExe%"
