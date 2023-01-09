7236ddbecb47b2d6c7d44d4a4660f95c7f78f78c
Deyan Dobromirov <dvd_video@abv.bg>
2023-01-09 11:37:07 +0200

    Fixed: Variable export skip "Path" Updated: Lua builder

--- RegistryMods/UserVariables/ExportVariables.bat
+++ RegistryMods/UserVariables/ExportVariables.bat
@@ -6,5 +6,11 @@ set "EXPORT_REG=HKEY_CURRENT_USER\Environment"
 reg import %EXPORT_PWD%\UserVariables.reg
 
 for /F "Tokens=1,2*" %%A in ('reg query %EXPORT_REG%') do (
-  If "%%B" neq "" setx "%%A" "%%C"
+  if "%%B" neq "" (
+    if "%%A" neq "Path" (
+      echo "%%A" : "%%C"
+      setx %%A "%%C"
+      set  "%%A=%%C"
+    )
+  )
 )
