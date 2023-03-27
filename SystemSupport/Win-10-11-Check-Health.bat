9cdb62e36fa936bc4c1baecf47a210c159bee10f
dvdvideo1234 <dvd_video@abv.bg>
2023-03-27 19:22:16 +0300

    Added: More restore control options

--- SystemSupport/Win-10-11-Check-Health.bat
+++ SystemSupport/Win-10-11-Check-Health.bat
@@ -2,6 +2,10 @@
 
 echo Restoring system health...
 
+call DISM /Online /Cleanup-Image /CheckHealth
+
+call DISM /Online /Cleanup-Image /ScanHealth
+
 call DISM /Online /Cleanup-image /Restorehealth
 
 call sfc /scannow
