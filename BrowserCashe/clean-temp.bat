c37f508ad1b95796bdcd705837d54c9b3beac6d2
Deyan Dobromirov <dvd_video@abv.bg>
2023-10-09 10:44:19 +0300

    Fixed: Remove everything not only c%

--- BrowserCashe/clean-temp.bat
+++ BrowserCashe/clean-temp.bat
@@ -1,22 +1,27 @@
 cd /d "C:%HOMEPATH%\local settings\temp"
-del c*.tmp
+del *.* /s /q
+rmdir . /s /q
 
 cd /d "C:%HOMEPATH%\local settings\tmp"
-del c*.tmp
+del *.* /s /q
+rmdir . /s /q
 
-cd /d %TMP%
-del c*.tmp
+cd /d "%TMP%"
+del *.* /s /q
+rmdir . /s /q
 
-cd /d %TEMP%
-del c*.tmp
+cd /d "%TEMP%"
+del *.* /s /q
+rmdir . /s /q
 
-cd /d  C:\windows\temp
-del c*.*
+cd /d  "C:\windows\temp"
+del *.* /s /q
+rmdir . /s /q
 
-cd /d C:\tmp
-del c*.*
+cd /d "C:\tmp"
+del *.* /s /q
+rmdir . /s /q
 
-cd /d C:\temp
-del c*.*
-
-exit 0
+cd /d "C:\temp"
+del *.* /s /q
+rmdir . /s /q
