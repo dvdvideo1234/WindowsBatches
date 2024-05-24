29950ebd504d9229a354e10f1558a843b05700ab
dvdvideo1234 <dvd_video@abv.bg>
2024-05-24 22:32:56 +0300

    Restart explorer after applying dark/light mode

--- SystemSupport/Win-10-11-Dark.bat
+++ SystemSupport/Win-10-11-Dark.bat
@@ -1,5 +1,9 @@
 @echo off
 
-call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force; New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
+call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
+call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme    -Value 0 -Type Dword -Force
+
+taskkill /f /im explorer.exe
+start explorer.exe
 
 exit 0
--- SystemSupport/Win-10-11-Light.bat
+++ SystemSupport/Win-10-11-Light.bat
@@ -1,5 +1,9 @@
 @echo off
 
-call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force; New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force
+call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force
+call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme    -Value 1 -Type Dword -Force
+
+taskkill /f /im explorer.exe
+start explorer.exe
 
 exit 0
