a8953326e375547da9f65b9e75935573bb86e3bf
Deyan Dobromirov <dvd_video@abv.bg>
2024-05-27 13:55:43 +0300

    Wait for the explorer shutdown to finish

--- SystemSupport/Win-10-11-Dark.bat
+++ SystemSupport/Win-10-11-Dark.bat
@@ -3,7 +3,7 @@
 call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
 call powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme    -Value 0 -Type Dword -Force
 
-taskkill /f /im explorer.exe
-start explorer.exe
+call taskkill /f /im explorer.exe
+call start explorer.exe
 
 exit 0
