5a1d04468a5dea80546eaf0a126380cc09b22c9b
dvdvideo1234 <dvd_video@abv.bg>
2026-01-15 19:48:38 +0200

    Added: Folder type enable/disable

--- SystemSupport/Win-10-11-Start-Export/export.bat
+++ SystemSupport/Win-10-11-Start-Export/export.bat
@@ -1,3 +1,8 @@
 @echo off
 
-call powershell -ExecutionPolicy Bypass -command "Export-StartLayout -Path StartLayout.xml"
+call powershell -ExecutionPolicy Bypass -command "Export-StartLayout -Path StartLayout.json"
+
+:: call powershell -ExecutionPolicy Bypass -command "Export-StartLayout -UseDesktopApplicationID -Path StartLayout.xml"
+
+:: %LocalAppData%\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState
+
