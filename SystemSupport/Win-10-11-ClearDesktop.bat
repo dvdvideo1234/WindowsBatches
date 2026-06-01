99250ea50cc0ba1c13cc49a844a3e29ea1c33ca5
Deyan Dobromirov <dvd_video@abv.bg>
2026-06-01 11:08:54 +0300

    Updated clear desktop

--- SystemSupport/Win-10-11-ClearDesktop.bat
+++ SystemSupport/Win-10-11-ClearDesktop.bat
@@ -2,14 +2,15 @@
 
 setlocal EnableDelayedExpansion
 
-set "RM_EXT_TEXT=txt c cpp h pc pcs pck bak xml har"
+set "RM_EXT_TEXT=txt c cpp h pc pcs pck bak xml html har lua"
 set "RM_EXT_WORD=xls xlsx doc docx ppt pptx rtf msg"
-set "RM_EXT_IMAG=bmp jpg jpeg png webp"
-set "RM_EXT_DATA=csv sql"
-set "RM_EXT_FILE=zip cia rar 7z arj"
+set "RM_EXT_IMAG=bmp jpg jpeg png webp gif"
+set "RM_EXT_DATA=csv sql log"
+set "RM_EXT_FILE=zip cia rar 7z arj md"
 set "RM_EXT_APPS=exe bin"
+set "RM_EXT_GITS=gitattributes gitignore"
 
-for %%C in (TEXT WORD IMAG DATA FILE APPS) do (
+for %%C in (TEXT WORD IMAG DATA FILE APPS GITS) do (
   set "RM_EXT_LST=!RM_EXT_%%C!"
   echo Remove [%%C] : !RM_EXT_LST!
 
