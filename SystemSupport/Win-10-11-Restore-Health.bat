25082e878743889ee080b4813ce021a49fca9158
dvdvideo1234 <dvd_video@abv.bg>
2026-09-05 22:59:03 +0300

    Added: Cleanup before running the repair as separate option

--- SystemSupport/Win-10-11-Restore-Health.bat
+++ SystemSupport/Win-10-11-Restore-Health.bat
@@ -4,36 +4,45 @@ set "DISM_BOOL="
 
 echo Restoring system health...
 
-call %SystemRoot%\system32\Dism.exe /Online /Cleanup-Image /CheckHealth
-
-call %SystemRoot%\system32\Dism.exe /Online /Cleanup-Image /ScanHealth
-
-call %SystemRoot%\system32\Dism.exe /Online /Cleanup-image /Restorehealth
-
-call %SystemRoot%\system32\Dism.exe /Online /Cleanup-Image /StartComponentCleanup
-
-call %SystemRoot%\system32\sfc.exe /scannow
-
-call %SystemRoot%\system32\sfc.exe /scannow
-
-call %SystemRoot%\system32\verifier.exe
-
-call %SystemRoot%\system32\sigverif.exe
-
-call %SystemRoot%\system32\dxdiag.exe
-
-:: Update or unstall WSL
-set /p "DISM_BOOL=Deep volume check [y/N]: "
+set /p "DISM_BOOL=Cleanup before start [y/N]: "
 if /I "%DISM_BOOL%" EQU "y" (
   set "DISM_BOOL=Y"
 )
 
 if /I "%DISM_BOOL%" EQU "Y" (
-  call %SystemRoot%\system32\chkdsk.exe /F /V /R C:
+  call %SystemRoot%\system32\Dism.exe /Online /Cleanup-Image /StartComponentCleanup
+  
+  call shutdown /r /t 60
 ) else (
-  call %SystemRoot%\system32\chkdsk.exe /I /C C:
+  call %SystemRoot%\system32\Dism.exe /Online /Cleanup-Image /CheckHealth
+
+  call %SystemRoot%\system32\Dism.exe /Online /Cleanup-Image /ScanHealth
+
+  call %SystemRoot%\system32\Dism.exe /Online /Cleanup-image /Restorehealth
+
+  call %SystemRoot%\system32\sfc.exe /scannow
+
+  call %SystemRoot%\system32\sfc.exe /scannow
+
+  call %SystemRoot%\system32\verifier.exe
+
+  call %SystemRoot%\system32\sigverif.exe
+
+  call %SystemRoot%\system32\dxdiag.exe
+
+  :: Update or unstall WSL
+  set /p "DISM_BOOL=Deep volume check [y/N]: "
+  if /I "%DISM_BOOL%" EQU "y" (
+    set "DISM_BOOL=Y"
+  )
+
+  if /I "%DISM_BOOL%" EQU "Y" (
+    call %SystemRoot%\system32\chkdsk.exe /F /V /R C:
+  ) else (
+    call %SystemRoot%\system32\chkdsk.exe /I /C C:
+  )
+
+  timeout 300
+
+  exit 0
 )
-
-timeout 300
-
-exit 0
